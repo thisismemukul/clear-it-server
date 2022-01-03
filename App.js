2c18ee7b7530062277b7625f056d028e004a32bf
Mukul Saini <thisismemukul@gmail.com>
2022-01-03 22:42:37 +0530

    cors

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 const dotenv = require("dotenv");
 
 dotenv.config({ path: './config.env' });
 require("./DB/conn");
 // const Users = require('./model/userSchema');
+
+app.use(cors());
+
 app.use(express.json());
 app.use(require('./router/auth'));
 const PORT = process.env.PORT;
